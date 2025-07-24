6aea09f3f442622d6a19d98b06fd7a1f9f0afd6c
Divine <nwokedidivine78@gmail.com>
2025-07-24 12:51:21 +0100

    finished

--- scripts/place.js
+++ scripts/place.js
@@ -1,10 +1,13 @@
-// Footer info
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// Wind Chill Function
 function calculateWindChill(tempF, speedMph) {
-  return (35.74 + 0.6215 * tempF - 35.75 * Math.pow(speedMph, 0.16) + 0.4275 * tempF * Math.pow(speedMph, 0.16)).toFixed(1);
+  return (
+    35.74 +
+    0.6215 * tempF -
+    35.75 * Math.pow(speedMph, 0.16) +
+    0.4275 * tempF * Math.pow(speedMph, 0.16)
+  ).toFixed(1);
 }
 
 const temp = parseFloat(document.getElementById("temp").textContent);
